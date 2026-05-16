724fc7d4244b0206f85bba97cff0f07502756add
AngadOnTop <angadjotd@gmail.com>
2026-05-16 13:18:15 +1000

    updated index.py

--- challenges/index.py
+++ challenges/index.py
@@ -1,26 +1,36 @@
-from numpy import character
 from cave import Cave
-from character import Character
+from character import Enemy
 
 cavern = Cave("Cavern")
-cavern.set_description("A damp and dity cave.")
+cavern.set_description("A dank and dirty cave")
 
 dungeon = Cave("Dungeon")
 dungeon.set_description("A large cave with a rack")
 
 grotto = Cave("Grotto")
-grotto.set_description("A small cave with ancient graffiti.")
+grotto.set_description("A small cave with ancient graffiti")
 
-# Linking
-dungeon.link_cave(grotto, "east")
 dungeon.link_cave(cavern, "north")
+dungeon.link_cave(grotto, "east")
 cavern.link_cave(dungeon, "south")
 grotto.link_cave(dungeon, "west")
 
+harry = Enemy("Harry", "A smelly Wumpus")
+harry.set_conversation("Hangry...Hanggrry")
+harry.set_weakness("vegemite")
+
+dungeon.set_character(harry)
+
+dead = False
 current_cave = cavern
 
-while True:
+while dead == False:
     print("\n")
     current_cave.get_details()
+
+    inhabitant = current_cave.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
+
     command = input("> ")
     current_cave = current_cave.move(command)
